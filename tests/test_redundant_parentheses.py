1ba779e021fe42992004740c7819b8c968469d41
Robsdedude <rouven.bauer@neo4j.com>
2022-09-21 13:21:51 +0200

    Fix yet another dump refactoring mistake

--- tests/test_redundant_parentheses.py
+++ tests/test_redundant_parentheses.py
@@ -686,7 +686,7 @@ def test_parens_in_slice_5(plugin):
 def test_if_with_parens_in_comprehension(plugin, comprehension_type):
     s = f"""{comprehension_type[0]}
     x
-    for x in range(10)""" + " " + """
+    for x in range(10)""" + " " + f"""
     if (some_super_mega_looooooooooooooooooooooooooooooooooooong_thing
         or some_other_super_mega_looooooooooooooooooooooong_thing)
 {comprehension_type[1]}
